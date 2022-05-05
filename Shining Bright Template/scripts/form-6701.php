5c3f4a30a60fa6b4ad7bab37354d2514d79630f7
fkynard2@gmail.com <fkynard2@gmail.com>
2022-05-05 03:38:47 -0400

    update 2.9

--- Shining Bright Template/scripts/form-6701.php
+++ Shining Bright Template/scripts/form-6701.php
@@ -5,7 +5,7 @@ require_once('FormProcessor.php');
 $form = array(
     'subject' => 'New Form Submission',
     'email_message' => 'You have a new form submission',
-    'success_redirect' => 'frank@kynard2.com',
+    'success_redirect' => '',
     'sendIpAddress' => true,
     'email' => array(
     'from' => 'frank@kynard2.com',
